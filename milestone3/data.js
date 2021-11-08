dd54136b5b2aad6f477988cd8037dd3e5a21b9a7
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-08 17:14:04 +0100

    melestone3

---
+++ milestone3/data.js
@@ -0,0 +1,164 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const form = document.querySelector('.form-select');
+let category;
+
+
+for (let key in icons) {
+
+	const icon = icons[key];
+
+	stampTemplate(icon);	
+
+	console.log('prima',category);
+
+	form.addEventListener("change", function() {
+
+		// console.log(category.classList.contains('animal'));
+	
+		if (form.value === 'animal') {
+			console.log('animale');
+			// console.log(category.classList.contains('animal'));
+			console.log('dopo',category);
+		} else if (form.value === 'vegetable') {
+			console.log('vegetable');
+		} else if (form.value === 'user') {
+			console.log('user');
+		}
+	})
+	
+}
+
+
+
+
+function stampTemplate(arreyObject) {
+
+	const {name, color, type} = arreyObject;
+
+	const sectionMain = document.querySelector('.ls-section-main');
+
+	category = document.createElement('div');
+	category.className = `col ls-padding ${type}`;
+
+	category.innerHTML = `
+		<div class="font-icoins-container">
+			<i class="fas fa-${name} ${color}"></i>
+			<span>${name}</span>
+	  </div>
+	`;
+	sectionMain.append(category);
+};
